1f6963f35460e42a4a6a9a7178bdcee3b4ae970e
mattcarp12 <mattcarp88@gmail.com>
2019-08-18 11:34:03 -0400

    complete Droid Cafe app

---
+++ Unit 2 - User Experience/Droid Cafe/app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -0,0 +1,21 @@
+package com.example.droidcafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Intent intent = getIntent();
+        String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView tvOrder = findViewById(R.id.tvOrder);
+        tvOrder.setText(message);
+    }
+}
