98833a1008cc6c42aef5d864045f9823832ea2a0
mattcarp12 <mattcarp88@gmail.com>
2019-09-04 07:18:36 -0400

    update

--- Unit 2 - User Experience/RecyclerView/app/src/main/java/com/example/recyclerview/WordListAdapter.java
+++ Unit 2 - User Experience/RecyclerView/app/src/main/java/com/example/recyclerview/WordListAdapter.java
@@ -1,5 +1,7 @@
 package com.example.recyclerview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,24 +9,37 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class WordListAdapter extends
         RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+
+    private final LinkedList<String> mWordList;
+    private LayoutInflater mInflator;
+
+    public WordListAdapter(Context context, LinkedList<String> wordList) {
+        mInflator = LayoutInflater.from(context);
+        this.mWordList = wordList;
+    }
 
 
     @NonNull
     @Override
     public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View mItemView = mInflator.inflate(R.layout.wordlist_item, parent, false);
+        return new WordViewHolder(mItemView, this);
     }
 
     @Override
     public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
-
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mWordList.size();
     }
 
     class WordViewHolder extends RecyclerView.ViewHolder {
---
+++ Unit 4 - Saving User Data/AppWithSettings/app/src/main/java/org/matt/appwithsettings/SettingsActivity.java
@@ -0,0 +1,17 @@
+package org.matt.appwithsettings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
